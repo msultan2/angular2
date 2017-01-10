c54545125e1ce47be9badfddfb1aabafc99c757e
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-01-10 12:13:23 +0000

    New 7. Nesting components and inputs feature

---
+++ src/main/webapp/7. Nesting components and inputs/script.ts
@@ -0,0 +1,61 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Component} from '@angular/core';
+
+
+class Joke{
+  setup: string;
+  punchline: string;
+  hide: boolean;
+
+  constructor(setup: string, punchline: string) {
+    this.setup = setup;
+    this.punchline = punchline;
+    this.hide = true;
+  }
+
+  toggle() {
+    this.hide = !this.hide;
+  }
+}
+
+let joke = new Joke("What did the cheese say when it looked in the mirror?","Hello-Me (Halloumi)");
+
+
+@Component({
+	selector: 'joke-list',
+	template: `
+<div class="card card-block"
+     *ngFor="let joke of jokes">
+	<h4 class="card-title">{{joke.setup}}</h4>
+	<p class="card-text"
+	   [hidden]="joke.hide">{{joke.punchline}}</p>
+	<button class="btn btn-primary"
+	    (click)="joke.toggle()">Tell Me</button>
+
+</div>
+  `
+  
+})
+class JokeListComponent {
+	jokes: Joke[];
+
+	constructor() {
+		this.jokes = [
+      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’")
+        ]
+  }
+}
+
+@NgModule({
+	imports: [BrowserModule],
+	declarations: [JokeListComponent],
+	bootstrap: [JokeListComponent]
+})
+export class AppModule {
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
