4a6d136b7283f17d260342599434a2454af279bb
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-01-10 12:15:27 +0000

    Upated Joke Component to render single joke.

--- src/main/webapp/7. Nesting components and inputs/script.ts
+++ src/main/webapp/7. Nesting components and inputs/script.ts
@@ -25,18 +25,18 @@ let joke = new Joke("What did the cheese say when it looked in the mirror?","Hel
 @Component({
   selector: 'joke',
   template: `
-    <h1>{{setup}}</h1>
-    <p>{{punchline}}</p>
+<div class="card card-block"
+	<h4 class="card-title">{{joke.setup}}</h4>
+	<p class="card-text"
+	   [hidden]="joke.hide">{{joke.punchline}}</p>
+	<button class="btn btn-primary"
+	    (click)="joke.toggle()">Tell Me</button>
+
+</div>
   `
 })
 class JokeComponent {
-  setup: string;        //Properties: Type
-  punchline: string;
-  
-  constructor(){
-    this.setup = "What did the cheese say when it looked in the mirror?";
-    this.punchline = "Halloumi (Hello Me)";
-  }
+  joke: Joke; 
 }
 
 @Component({
