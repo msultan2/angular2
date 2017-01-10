d7b0dbd2f7884764ce0d0f889919e41280e1495a
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-01-10 10:33:32 +0000

    Added Event "click" to button.

--- src/main/webapp/5. Property and event binding/script.ts
+++ src/main/webapp/5. Property and event binding/script.ts
@@ -11,7 +11,8 @@ import {Component} from '@angular/core';
 	<h4 class="card-title">{{joke.setup}}</h4>
 	<p class="card-text"
 	   [hidden]="joke.hide">{{joke.punchline}}</p>
-	<button class="btn btn-warning">Tell Me</button>
+	<button class="btn btn-primary"
+	    (click)="joke.hide=!joke.hide">Tell Me</button>
 
 </div>
   `
