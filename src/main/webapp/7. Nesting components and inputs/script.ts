dc7571de6f57d797007a5d7f988bb2de9994a874
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-01-10 12:17:48 +0000

    Updated Joke-list.

--- src/main/webapp/7. Nesting components and inputs/script.ts
+++ src/main/webapp/7. Nesting components and inputs/script.ts
@@ -42,15 +42,7 @@ class JokeComponent {
 @Component({
 	selector: 'joke-list',
 	template: `
-<div class="card card-block"
-     *ngFor="let joke of jokes">
-	<h4 class="card-title">{{joke.setup}}</h4>
-	<p class="card-text"
-	   [hidden]="joke.hide">{{joke.punchline}}</p>
-	<button class="btn btn-primary"
-	    (click)="joke.toggle()">Tell Me</button>
-
-</div>
+        <joke *ngFor="let j of jokes"></joke> 
   `
   
 })
